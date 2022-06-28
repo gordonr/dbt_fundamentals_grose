4ef6662c08b599beb57b906c2265a07b89f043cc
Gordon Rose <gordon.rose@fishtownanalytics.com>
2022-06-28 18:20:22 +0000

    First commit!

---
+++ models/example/my_first_model.sql
@@ -0,0 +1,21 @@
+/*
+{{
+    config(
+        materialized = 'view',
+        tags=['finance']
+    )
+}}
+*/
+
+select 
+    1 as id,
+    1 / 2 as my_ratio
+
+/*
+union all
+
+select 
+    null as id,
+    1 / 3 as my_ratio
+*/   
+
---
+++ models/example/my_second_model.sql
@@ -0,0 +1,10 @@
+
+-- Use the `ref` function to select from other models
+
+select 
+    *,
+    current_timestamp as _loaded_at
+from 
+    {{ ref('my_first_model') }}
+where
+    id = 1
