b409f836edebc0d15485ddec0c53795380ba5b6b
Gordon Rose <gordon.rose@fishtownanalytics.com>
2022-07-06 00:50:43 +0000

    Cleaned up lifetime value and dim_customers models, added macro, package

---
+++ macros/grant_select.sql
@@ -0,0 +1,14 @@
+-- e.g. dbt run-operation grant_select --args '{"role":"transformer"}'
+
+{% macro grant_select(role) %}
+{% set sql %}
+    grant usage on schema {{ target.schema }} to role {{ role }};
+    grant select on all tables in schema {{ target.schema }} to role {{ role }};
+    grant select on all views in schema {{ target.schema }} to role {{ role }};
+{% endset %}
+
+{% do run_query(sql) %}
+{% do log("Privileges granted", info=True) %}
+{% endmacro %}
+
+
--- models/marts/core/fct_lifetime_value.sql
+++ models/marts/core/fct_lifetime_value.sql
@@ -1,52 +1,18 @@
-with customers as (
-
-    select * from {{ ref('stg_customers')}}
-
-),
-
-orders as (
+with orders as (
 
     select * from {{ ref('stg_orders') }}
 
 ),
 
-lifetime_value as (
+payments as (
 
-    select * from {{ ref('fct_lifetime_value') }}
-
-),
-
-customer_orders as (
-
-    select
-        customer_id,
-
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
-    from orders
-
-    group by 1
-
-),
-
-final as (
-
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        coalesce(lv.lifetime_value, 0) as lifetime_value
-
-    from customers
-
-    left join customer_orders using (customer_id)
-    left join lifetime_value lv using (customer_id)
+    select * from {{ ref('stg_payments') }}
 
 )
 
-select * from final
+select 
+    o.customer_id, sum(p.amount) as lifetime_value
+from   
+    orders o inner join 
+        payments p on o.order_id = p.order_id
+group by o.customer_id
