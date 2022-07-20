b160d6c98613ee8c93c2c8a194cd378a1fecfb10
Gordon Rose <gordon.rose@fishtownanalytics.com>
2022-07-20 01:25:32 +0000

    Updated test

--- tests/assert_positive_total_for_payments.sql
+++ tests/assert_positive_total_for_payments.sql
@@ -2,7 +2,7 @@
 -- Therefore return records where this isn't true to make the test fail.
 select
     order_id,
-    sum(amount) as total_amount
+    sum(payment_amount) as total_amount
 from {{ ref('stg_payments') }}
 group by 1
 having not(total_amount >= 0)
