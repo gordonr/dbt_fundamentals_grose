c134ffde865c83d661586e03fe1aa421716df415
Gordon Rose <gordon.rose@fishtownanalytics.com>
2022-09-08 19:11:45 +0000

    Updated stg payments

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -4,7 +4,8 @@ select
     paymentmethod as payment_method,
     status,
     -- amount is stored in cents, convert it to dollars
-    {{ cents_to_dollars('amount') }} as payment_amount,
+    -- {{ cents_to_dollars('amount') }} as payment_amount,
+    amount as payment_amount,
     created as created_at
 
 --from raw.stripe.payment
