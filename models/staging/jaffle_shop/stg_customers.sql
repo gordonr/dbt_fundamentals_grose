473884a4d22e42e298b76c99747765d57f953ce1
Gordon Rose <gordon.rose@fishtownanalytics.com>
2022-09-21 13:50:19 +0000

    Update

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -5,3 +5,5 @@ select
 
 --from raw.jaffle_shop.customers
 from {{ source('my_jaffle_shop', 'customers') }}
+
+ -- dbt run-operation generate_base_model --args '{"source_name": "my_jaffle_shop", "table_name": "customers"}'
