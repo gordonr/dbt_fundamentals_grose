371fc56b1cc15aca086ba0a4ee7a4e0afdae0c1e
Gordon Rose <gordon.rose@fishtownanalytics.com>
2023-04-07 17:51:21 +0000

    Commit

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,12 +1,12 @@
 select
     id as payment_id,
     orderid as order_id,
-    paymentmethod as payment_method,
+    -- paymentmethod as payment_method,
     status,
     -- amount is stored in cents, convert it to dollars
     -- amount as payment_amount,
-    {{ cents_to_dollars('amount') }} as payment_amount,
-    -- amount/100 as payment_amount,
+    -- {{ cents_to_dollars('amount') }} as payment_amount,
+    amount/100 as payment_amount,
     created as created_at 
 
 from {{ source('my_stripe', 'payment') }}
