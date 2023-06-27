3cc502d3bf48bc87d35d782d948812c9f1c4a534
Gordon Rose <gordon.rose@fishtownanalytics.com>
2023-06-27 13:32:15 +0000

    Commit

--- models/marts/core/fct_lifetime_value.sql
+++ models/marts/core/fct_lifetime_value.sql
@@ -23,7 +23,7 @@ payments as (
 )
 
 select 
-    o.customer_id, sum(p.payment_amount) as lifetime_value, x
+    o.customer_id, sum(p.payment_amount) as lifetime_value
 from   
     orders o inner join 
         payments p on o.order_id = p.order_id
