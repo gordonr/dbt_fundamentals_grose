e97338d5439f87bfb316956b47fd8c406bcf1777
Gordon Rose <gordon.rose@fishtownanalytics.com>
2023-12-07 18:46:45 +0000

    Commit

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -15,10 +15,10 @@ select
     status as status,
     
     -- amount   is stored in cents. We need to convert it to dollars 
-    amount as payment_amount,  
+    --amount as payment_amount,  
 
     --{ { cents_to_dollars('amount') }} as payment_amount,
-    -- amount/100 as payment_amount,
+    amount/100 as payment_amount,
     created as created_at
 
 from {{ source('my_stripe', 'payment') }}
