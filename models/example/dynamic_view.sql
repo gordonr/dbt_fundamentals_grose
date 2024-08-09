7bbfd30120458393fb036f83fda7a0bc828cd731
Gordon Rose <gordon.rose@fishtownanalytics.com>
2024-08-09 14:57:18 +0000

    Commit

--- models/example/dynamic_view.sql
+++
@@ -1,9 +0,0 @@
-{{ config(
-    materialized="dynamic_table",
-    on_configuration_change= "apply",
-    target_lag= "12 hours",
-    snowflake_warehouse="TRANSFORMING"
-    ) 
-}}
-
-select * from {{ ref("stg_customers") }}   
