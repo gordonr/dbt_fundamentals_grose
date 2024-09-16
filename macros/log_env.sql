a5499dd542305ace291e6b3c45cc5727a818dcfe
Gordon Rose <gordon.rose@fishtownanalytics.com>
2024-09-16 18:54:43 +0000

    Commit

---
+++ macros/log_env.sql
@@ -0,0 +1,8 @@
+  {% macro log_env() %}
+      {{ log('\nGenerating cleanup queries...\n', info=True) }}
+      {{ log("DBT_ENV                 : " ~ env_var('DBT_ENV', 'NOT DEFINED') ~ '\n', info=True) }}
+      {{ log("DBT_CLOUD_JOB_ID        : " ~ env_var('DBT_CLOUD_JOB_ID', 'NOT DEFINED') ~ '\n', info=True) }}
+      {{ log("DBT_CLOUD_ENVIRONMENT_ID: " ~ env_var('DBT_CLOUD_ENVIRONMENT_ID', 'NOT DEFINED') ~ '\n', info=True) }}
+      {{ log("DBT_CLOUD_PR_ID         : " ~ env_var('DBT_CLOUD_PR_ID', 'NOT DEFINED') ~ '\n', info=True) }}
+      {{ log("DBT_CLOUD_RUN_ID        : " ~ env_var('DBT_CLOUD_RUN_ID', 'NOT DEFINED') ~ '\n', info=True) }}
+  {% endmacro %}
