d19724362edb22be19b9efbb116ca59394654da5
Gordon Rose <gordon.rose@fishtownanalytics.com>
2024-10-17 13:37:49 +0000

    Commit

--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,14 +1,13 @@
-select
-    id as order_id,
-    user_id as customer_id,
-    TO_DATE(order_date) as order_date,
-    datediff('day', order_date, {{ dbt.current_timestamp() }} ) as days_since_ordered,
-    status like '%pending%' as is_status_pending,
-    case 
-        when status like '%return%' then 'returned'
-        when status like '%pending%' then 'placed'
-        else status
-    end as status
+SELECT
+    id AS order_id,
+    user_id AS customer_id,
+    TO_DATE(order_date) AS order_date,
+    DATEDIFF('day', order_date, {{ dbt.current_timestamp() }} ) AS days_since_ordered,
+    status LIKE '%pending%' AS is_status_pending,
+    CASE 
+        WHEN status LIKE '%return%' THEN 'returned'
+        WHEN status LIKE '%pending%' THEN 'placed'
+        ELSE status
+    END AS status
 
---from raw.jaffle_shop.orders
-from {{ source('my_jaffle_shop', 'orders') }}
+FROM {{ source('my_jaffle_shop', 'orders') }}
